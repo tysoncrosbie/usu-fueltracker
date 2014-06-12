c34eca1a152f3370af5a307b70d1cb8373531ba3
Tyson Crosbie <tyson@tysoncrosbie.com>
2014-06-12 17:39:32 -0600

    add production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
-worker_processes ENV['UNICORN_WORKERS'] && ENV['UNICORN_WORKERS'].to_i || 3
-timeout          ENV['UNICORN_TIMEOUT'] && ENV['UNICORN_TIMEOUT'].to_i || 15
-preload_app      true
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -8,27 +8,15 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  if defined?(ActiveRecord::Base)
-    config = Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] && ENV['DB_REAP_FREQ'].to_i || 10 # seconds
-    config['pool']              = ENV['DB_POOL'] && ENV['DB_POOL'].to_i || 2
-
-    ActiveRecord::Base.establish_connection(config)
-  end
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
