649e930e264c6c51f44a354e1bc38ab310ddce86
Tyson Crosbie <tyson@tysoncrosbie.com>
2014-07-14 18:30:05 -0600

    500 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  unless Rails.configuration.consider_all_requests_local
-    rescue_from Exception, with: :render_500
-
-    rescue_from CanCan::AccessDenied do |exception|
-      unauthorized_redirect(exception)
-    end
-
-    rescue_from AbstractController::ActionNotFound,  with: :render_404
-    rescue_from ActiveRecord::RecordNotFound,        with: :render_404
-    rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from ActionController::RoutingError,      with: :render_404
-    rescue_from URI::InvalidURIError,                with: :render_404
+  rescue_from CanCan::AccessDenied do |exception|
+    unauthorized_redirect(exception)
   end
-
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
