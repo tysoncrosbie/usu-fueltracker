4e2a4ff43507945787622601a4c5ebcba65156a3
Tyson Crosbie <tyson@tysoncrosbie.com>
2021-03-27 13:07:12 -0700

    Create manifest.js
    new config for assets

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,13 @@
+//= link_tree ../admin/images
+//= link_directory ../admin/javascripts .js
+//= link_directory ../admin/stylesheets .css
+
+
+  // #  the old config for future reference
+  //
+  // # config.assets.precompile += %w(
+  // #   active_admin.js
+  // #   active_admin.css
+  // #   compatibility/html5shiv
+  // #   compatibility/respond.min
+  // # )
